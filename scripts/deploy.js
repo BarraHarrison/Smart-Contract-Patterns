c4595d750ffa51bf2f646840ee1afe7aeb772d60
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-11-18 10:44:18 +0900

    Getting the deployer address and compiling the contract

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+    console.log("Deploying contract with:", deployer.address);
+
+    const TipJarOwnable = await hre.ethers.getContractFactory("TipJarOwnable");
+
+    const tipJar = await TipJarOwnable.deploy();
+    await tipJar.waitForDeployment();
+
+    console.log("TipJarOwnable deployed to:", await tipJar.getAddress());
+}
+
